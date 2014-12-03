3527bdb6a54df1eb090ed38bcac75632c245a978
rachbrown151 <rabrown@seas.upenn.edu>
2014-12-02 19:35:07 -0500

    Update trip.py
    spacing problems

--- api/trip.py
+++ api/trip.py
@@ -86,34 +86,34 @@ elif validate_token(username, post('token')):
             data['message'] = 'Insufficient information given'
     elif action == 'add_expense':
         if has_fields(['id']):
-		    trip_id = post('id')
-		    description = None
-		    cost = None
-		    if has_fields(['description']):
-			    description = post('description')
-		    if has_fields(['cost']):
-			    cost = post('cost')
-		    execute_query("INSERT INTO expenses (trip_id, user_id, description, cost) VALUES (\"%s\", \"%s\", \"%s\", \"%s\")" % (trip_id, None, description, cost))
-		    data['status'] = 'Success'
+	    trip_id = post('id')
+	    description = None
+	    cost = None
+	    if has_fields(['description']):
+	        description = post('description')
+	    if has_fields(['cost']):
+	        cost = post('cost')
+	    execute_query("INSERT INTO expenses (trip_id, user_id, description, cost) VALUES (\"%s\", \"%s\", \"%s\", \"%s\")" % (trip_id, None, description, cost))
+	    data['status'] = 'Success'
             data['message'] = 'Added expense'
         else:
             data['status'] = 'Failure'
             data['message'] = 'Insufficient information given'
     elif action == 'claim_expense':
         if has_fields('expense_id', 'expense_user'):
-		    expense_id = post('expense_id')
-		    expense_user = post('expense_user')
-		    execute_query("UPDATE expenses SET user_id = \"%s\" WHERE expenses.id = \"%s\"" % (expense_user, expense_id))
-		    data['status'] = 'Success'
+	    expense_id = post('expense_id')
+	    expense_user = post('expense_user')
+	    execute_query("UPDATE expenses SET user_id = \"%s\" WHERE expenses.id = \"%s\"" % (expense_user, expense_id))
+	    data['status'] = 'Success'
             data['message'] = 'Claimed expense'
         else:
             data['status'] = 'Failure'
             data['message'] = 'Insufficient information given'
     elif action == 'remove_expense':
         if has_fields('expense_id'):
-		    expense_id = post('expense_id')
-		    execute_query("DELETE FROM expenses WHERE id = \"%s\"" % (expense_id))
-		    data['status'] = 'Success'
+	    expense_id = post('expense_id')
+	    execute_query("DELETE FROM expenses WHERE id = \"%s\"" % (expense_id))
+	    data['status'] = 'Success'
             data['message'] = 'Removed expense'
         else:
             data['status'] = 'Failure'
