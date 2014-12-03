8ee2600c6b45b70448b1ba3f5a9412688e20e878
mdmorant <mmorant@seas.upenn.edu>
2014-12-03 14:44:54 -0500

    Added functions content.py

--- api/content.py
+++ api/content.py
@@ -60,16 +60,54 @@ elif (validate_token(post('user_id'), post('token'))):
 		else:
 			data['status'] = 'failure'
 			data['message'] = 'Failure, no album_id or privacy submitted'
+	elif action = 'get_content_info':
+		if has_fields(['content_id']):
+			content_id = post('content_id')
+			content = {}
+			content_info = execute_query("Select * From content Where id content.id = \"%s\"" % (content_id))
+			if len(query) > 0:
+				content['status'] = 'Success'
+				content['id'] = content_id
+				content['album_id'] = content_info[0][1]
+				content['location_id'] = content_info[0][2]
+				content['url'] = content_info[0][3]
+				content['type'] = content_info[0][4]
+				content['likes'] = execute_query("Select user_id From content_likes Where content_id = \"%s\"" % (content_id))
+				content['comments'] = execute_query("Select user_id, comment From content_comments Where content_id = \"%s\"" % (content_id))
+				
+			else:
+				content['status'] = 'Failure'
+				content['message'] = 'No content found with given id'
+			data['content'] = content
+		else:
+			data['status'] = 'Failure'
+			data['message'] = 'No content_id given'
 	
-	
+	elif action = 'tag_content_location'
+		if has_fields(['content_id', 'location_name'])
+			content_id = post('content_id')
+			location_name = post('location_name')
+			query = execute_query("Select id From locations Where citystate Like '%\"%s\"%'" % (location_name))
+			if len(query) > 0:
+				location_id = query[0][0]
+				query = execute_query("UPDATE content SET location_id =  \"%s\" WHERE id =  \"%s\"" % (location_id,content_id))
+				data['status'] = "Success"
+				data['message'] = "Updated content location"
+			else:
+				data['status'] = "Failure"
+				data['message'] = "No location found with that name"
+		else:
+			data['status'] = "Failure"
+			data['message'] = "No content_id or location_name given"
+			
 else:
     data['status'] = 'Failure'
     data['message'] = 'Token authentication failed. Token may have expired.'
 export_json(data)
 """add album to trip (done but not tested)
-get content info (likes / comments)
-add content to album (done but not tested)
-tag content location
+get content info (likes / comments) (done but not tested)
+add content to album (tested)
+tag content location  (done but not tested)
 like content (tested)
 comment on content (tested)
 set privacy (tested)
