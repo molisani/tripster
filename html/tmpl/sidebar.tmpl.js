d16e2d393794b2f57c2ce89677204f1275a8a210
Michael Molisani <molisani@seas.upenn.edu>
2014-12-03 21:52:21 -0500

    Mostly formatting, tried to simplify things

--- html/tmpl/sidebar.tmpl.js
+++ html/tmpl/sidebar.tmpl.js
@@ -6,7 +6,7 @@ templates['sidebar.tmpl'] = template({"1":function(depth0,helpers,partials,data)
   if (stack1 != null) { buffer += stack1; }
   return buffer + "    <script type=\"text/javascript\">\r\n    $('#logout-btn').click(function(){\r\n        $.ajax({\r\n            url: '../api/user.py', \r\n            type: 'post',\r\n            data: {\r\n                action: 'logout',\r\n                user_id: $.cookie('user_id'),\r\n                token: $.cookie('token')\r\n            },\r\n            dataType: 'json',\r\n            success: function(json) {\r\n                $.cookie('username', '', {expires: -1});\r\n                $.cookie('user_id', '', {expires: -1});\r\n                $.cookie('token', '', {expires: -1});\r\n                location.reload();\r\n            }\r\n        });\r\n    });\r\n    </script>\r\n";
 },"2":function(depth0,helpers,partials,data) {
-  var stack1, buffer = "        <div class=\"panel-body\">\r\n            <hr>\r\n            <span class=\"label label-primary pull-right\">Friends</span>\r\n        </div>\r\n        <table class=\"table\">\r\n            <thead></thead>\r\n            <tbody>\r\n";
+  var stack1, buffer = "        <div class=\"panel-body\" style=\"padding-top: 0px;\">\r\n            <hr style=\"margin-top: 5px;\"></hr>\r\n            <span class=\"label label-primary\">Friends</span>\r\n        </div>\r\n        <table class=\"table\">\r\n            <thead></thead>\r\n            <tbody>\r\n";
   stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.friends : depth0), {"name":"each","hash":{},"fn":this.program(3, data),"inverse":this.noop,"data":data});
   if (stack1 != null) { buffer += stack1; }
   return buffer + "            </tbody>\r\n        </table>\r\n";
