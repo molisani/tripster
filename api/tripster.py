bb1a8ea7b89a3d9902d215cb4e1d9991403fedee
mdmorant <mmorant@seas.upenn.edu>
2014-12-07 11:09:13 -0500

    Added location recs

--- api/tripster.py
+++ api/tripster.py
@@ -49,10 +49,22 @@ elif (validate_token(post('user_id'), post('token'))):
         data['status'] = 'Failure'
         data['message'] = 'No action specified' 
 		
-	"""
-	elif action = 'recommend_locations':
-		query = execute_query("Select Distinct V.location_id From takes T Inner Join ( Select Distinct F.user2_id From users U Inner Join friends F on U.id = F.user1_id Where U.id = 1) FR on FR.user2_id = T.user_id Inner Join visits V on V.trip_id = T.trip_id Where V.location_id Not In  (Select Distinct V.location_id From users U Inner Join takes T on T.user_id = U.id Inner Join visits V on V.trip_id = T.trip_id Where U.id = \"%s\");" % (user_id))
-	"""
+	
+	elif action == 'recommend_locations':
+		location_query = execute_query("Select id, rating From locations l Inner Join (Select Distinct V.location_id From takes T Inner Join ( Select Distinct F.user2_id From users U Inner Join friends F on U.id = F.user1_id Where U.id = \"%s\") FR on FR.user2_id = T.user_id Inner Join visits V on V.trip_id = T.trip_id Where V.location_id Not In  (Select Distinct V.location_id From users U Inner Join takes T on T.user_id = U.id Inner Join visits V on V.trip_id = T.trip_id Where U.id = \"%s\")) locs on l.id = locs.location_id Order By rating Desc" % (user_id))
+		if len(query) > 0:	
+			rec_locs = {}
+			locations = []
+				for location in location_query:
+					l = {}
+					l['location_id'] = location[0]
+					locations+= [l]
+			num_results = min (len(locations),5)
+			data['status'] = 'Success'
+			data['locations'] = locations[0:num_results]
+		else:
+			data['status'] = 'Failure'
+			data['message'] = 'No available recommended locations'
 else:
     data['status'] = 'Failure'
     data['message'] = 'Token authentication failed. Token may have expired.'
