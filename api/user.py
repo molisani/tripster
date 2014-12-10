bd666d94ca1edb07845db3afd2e64728c8383710
rachbrown151 <rabrown@seas.upenn.edu>
2014-12-09 20:04:24 -0500

    Update user.py
    Added to user info

--- api/user.py
+++ api/user.py
@@ -51,6 +51,11 @@ elif has_fields(['user_id', 'token']):
                     user['id'] = post('id')
                     user['username'] = info[0][1]
                     user['fullname'] = info[0][3]
+                    user['birthday'] = info[0][6]
+                    user['email'] = info[0][7]
+                    user['aboutme'] = info[0][8]
+                    user['interests'] = info[0][9]
+                    user['affiliation'] = info[0][10]
                     data['user'] = user
             else:
                 data['status'] = 'Failure'
