3c52a15a26ae4ce0989ad12948fb3a160f098e19
Michael Molisani <molisani@seas.upenn.edu>
2014-12-09 20:30:05 -0500

    Added result modal to sidebar, needs to be added to each page post-body

--- html/tmpl/result-modal.tmpl.js
+++ html/tmpl/result-modal.tmpl.js
@@ -1,13 +1,9 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['result-modal.tmpl'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  var stack1, helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, lambda=this.lambda;
-  return "<div class=\"modal fade\" tabindex=\"-1\" role=\"dialog\" id=\""
-    + escapeExpression(((helper = (helper = helpers.modal_id || (depth0 != null ? depth0.modal_id : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"modal_id","hash":{},"data":data}) : helper)))
-    + "\" aria-hidden=\"true\">\r\n    <div class=\"modal-dialog modal-sm\">\r\n        <div class=\"modal-content\">\r\n            <div class=\"text-center\" style=\"padding: 10px;\"><b>"
-    + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.result : depth0)) != null ? stack1.status : stack1), depth0))
-    + "</b>: "
-    + escapeExpression(lambda(((stack1 = (depth0 != null ? depth0.result : depth0)) != null ? stack1.message : stack1), depth0))
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<div class=\"modal fade\" tabindex=\"-1\" role=\"dialog\" id=\"result-modal\">\r\n    <div class=\"modal-dialog modal-sm\">\r\n        <div class=\"modal-content\">\r\n            <div class=\"text-center\" style=\"padding: 10px;\">"
+    + escapeExpression(((helper = (helper = helpers.message || (depth0 != null ? depth0.message : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"message","hash":{},"data":data}) : helper)))
     + "</div>\r\n        </div>\r\n    </div>\r\n</div>";
 },"useData":true});
 })();
