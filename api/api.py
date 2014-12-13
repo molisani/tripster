06083f8bfa67d7e321f1091fb0f9bd1e80dd5e65
Michael Molisani <molisani@seas.upenn.edu>
2014-12-13 04:04:32 -0500

    Added listing functions for users, trips, and locations. Started privacy, not tested

--- api/api.py
+++ api/api.py
@@ -7,16 +7,16 @@ cgitb.enable()
 
 post_fields = cgi.FieldStorage()
 
-# db = MySQLdb.connect(
-#     host="***REMOVED***",
-#     user="cis450",
-#     passwd="***REMOVED***",
-#     db="MyDB")
-
 db = MySQLdb.connect(
-    unix_socket='/cloudsql/tripster-fall14:db-1',
-    user='root',
-    db='MyDB')
+    host="***REMOVED***",
+    user="cis450",
+    passwd="***REMOVED***",
+    db="MyDB")
+
+# db = MySQLdb.connect(
+#     unix_socket='/cloudsql/tripster-fall14:db-1',
+#     user='root',
+#     db='MyDB')
 
 db.autocommit(True)
 
@@ -65,44 +65,85 @@ def validate_token(user_id, token):
     now = datetime.now() - timedelta(hours=1)
     res = execute_query("SELECT users.token_gen FROM users WHERE users.id = \"%s\" AND users.token = \"%s\"" % (user_id, token))
     if len(res) != 1: return False
-    else: return res[0][0] > now
+    else: return res[0][0] > now 
 
-def table_permissions(user_id, table, table_id, edit):
-    creator_query = execute_query("SELECT * FROM %s WHERE %s.id = \"%s\" AND %s.creator_id = \"%s\"" % (table, table, table_id, table, user_id))
-    if len(creator_query) > 0:
-        return True
-    elif edit:
-        return False
-    privacy_query = execute_query("SELECT %s.privacy FROM %s WHERE %s.id = \"%s\"" % (table, table, table, table_id))
-    if len(privacy_query) == 0:
-        return False
-    privacy = privacy_query[0][0]
-    if privacy == 0:
-        return True
-    elif privacy == 1:
-        if table == 'trips':
-            trip_id = table_id
-        elif table == 'albums':
-            trip_query = execute_query("SELECT trips.id FROM trips INNER JOIN albums ON albums.trip_id = trips.id WHERE albums.id = \"%s\"" % (table_id))
-            if len(trip_query) == 0:
+def list_users():
+    users = []
+    for u in execute_query("SELECT id, username, fullname FROM users"):
+        user = {
+            'user_id': u[0],
+            'username': u[1],
+            'fullname': u[2]
+        }
+        users += [user]
+    return users
+
+def list_trips():
+    trips = []
+    for t in execute_query("SELECT id, tripname FROM trips"):
+        trip = {
+            'id': t[0],
+            'tripname': t[1]
+        }
+        trips += [trip]
+    return trips
+
+def list_locations():
+    locations = []
+    for l in execute_query("SELECT id, locationname, country FROM locations"):
+        location = {
+            'id': l[0],
+            'locationname': l[1],
+            'country': l[2]
+        }
+        locations += [location]
+    return locations
+
+def has_permissions(user_id, table, id, add=False, edit=False):
+    if edit:
+        return len(execute_query("SELECT * FROM %s WHERE id = \"%s\" AND creator_id = \"%s\"" % (table, id, user_id))) > 0
+    else:
+        if table == "content":
+            id = execute_query("SELECT album_id FROM content WHERE id = \"%s\"" % (id))[0][0]
+            table = "albums"
+        if table == "albums":
+            result = execute_query("SELECT trip_id, privacy FROM albums WHERE id = \"%s\"" % (id))
+            id = result[0][0]
+            privacy = result[0][1]
+            if privacy == 0:
+                return not add
+            elif privacy == 1:
+                status = execute_query("SELECT status FROM takes WHERE trip_id = \"%s\" AND user_id = \"%s\"" % (id, user_id))
+                if len(status) > 0:
+                    if add:
+                        return status[0][0] == 0
+                    else:
+                        return status[0][0] == 0 or status[0][0] == 1
+                else:
+                    return False
+            elif privacy == 2:
+                return len(execute_query("SELECT * FROM albums WHERE id = \"%s\" AND creator_id = \"%s\"" % (id, user_id))) > 0
+            else:
                 return False
-            trip_id = trip_query[0][0]
-        else:
-            return False
-        takes_query = execute_query("SELECT takes.status FROM takes WHERE takes.trip_id = \"%s\" AND takes.user_id = \"%s\"" % (trip_id, user_id))
-        if len(takes_query) == 0:
-            return False
-        return takes_query[0][0] == 0
-    else: 
+        if table == "trip":
+            result = execute_query("SELECT privacy FROM trips WHERE id = \"%s\"" % (id))
+            privacy = result[0][0]
+            if privacy == 0:
+                return not add
+            elif privacy == 1:
+                status = execute_query("SELECT takes.status FROM takes WHERE trip_id = \"%s\" AND user_id = \"%s\"" % (id, user_id))
+                if len(access) > 0:
+                    if add:
+                        return status[0][0] == 0
+                    else:
+                        return status[0][0] == 0 or status[0][0] == 1
+                else:
+                    return False
+            elif privacy == 2:
+                return len(execute_query("SELECT * FROM trips WHERE id = \"%s\" AND creator_id = \"%s\"" % (id, user_id))) > 0
+            else:
+                return False
         return False
-
-def has_permissions(user_id, album=None, trip=None, edit=False):
-    if album is not None:
-        return table_permissions(user_id, "albums", album, edit)
-    elif trip is not None:
-        return table_permissions(user_id, "trips", trip, edit)
-    else: 
-        return False    
 
 def export_json(data={}, success=True, message=None):
     data['status'] = 'Success' if success else 'Failure'
