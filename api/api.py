32d07cb378d7e830f99645a64359d05edb29b9d3
Michael Molisani <molisani@seas.upenn.edu>
2015-02-10 17:27:41 -0500

    Restored database connection comments
    Just in case anyone wants to run it locally.

--- api/api.py
+++ api/api.py
@@ -17,6 +17,12 @@ cgitb.enable()
 
 post_fields = cgi.FieldStorage()
 
+# db = MySQLdb.connect(		
+#     host="",		
+#     user="",		
+#     passwd="",		
+#     db="")
+
 db = MySQLdb.connect(
     unix_socket='/cloudsql/tripster-fall14:db-1',
     user='root',
